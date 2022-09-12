8f3ff5ac169f023c867293b094dfd02b328d1f50
v.gulukov <vit1c@yandex.ru>
2022-09-12 16:14:39 +0300

    //new changes for publishing on GitHub

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,6 +2,7 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
+        //new changes for publishing on GitHub
         System.out.println("Hello world");
         System.out.println("Привет мир");
     }
