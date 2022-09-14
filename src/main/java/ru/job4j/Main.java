038530c574b4278b43175e869de6e9a9158d2761
v.gulukov <vit1c@yandex.ru>
2022-09-14 17:19:27 +0300

    1. Определите тип данных для переменных. [#172634]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -4,8 +4,30 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Привет мир");
-        int age = 18;
-        float length = 78.0332F;
-        boolean baby = true;
+        //age это возраст
+        int age;
+
+        //sex это пол
+        char sex;
+
+        //height это рост
+        //по задаче измерение в метрах, поэтому float с возможностью десятичных дробей
+        float height;
+
+        //bricksAmount это количество кирпичей для постройки дома (небольшой одноэтажный дом площадью 100м2)
+        short bricksAmount;
+
+        //bactAmount это количество бактерий (измерять будем в КОЕ / г - такую величину можно встретить если почитать состав обычного йогурта,
+        // часто там можно встретить число 1 * 107 КОЕ / г)
+        float bactAmount;
+
+        //spaceDistance это расстояние до космических объектов (измерять будем в км)
+        double spaceDistance;
+
+        //workLicense это разрешение на работу (рассматриваем всего 2 значения - или есть разрешение или его нет)
+        boolean workLicense;
+
+        //fileSize это размер файла (измерять будем в байтах)
+        double fileSize;
     }
 }
