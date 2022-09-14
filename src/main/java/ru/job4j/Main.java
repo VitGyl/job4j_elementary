34a4954dbb607c21505bddf378671af36b06c879
v.gulukov <vit1c@yandex.ru>
2022-09-14 09:04:17 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,7 +2,6 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        //new changes for publishing on GitHub
         System.out.println("Hello world");
         System.out.println("Привет мир");
     }
