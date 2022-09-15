261cca742e73ffcfbae1b98e0cc3115eff1b62b3
v.gulukov <vit1c@yandex.ru>
2022-09-15 10:19:48 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -5,29 +5,29 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Привет мир");
         //age это возраст
-        int age;
+        int age = 49;
 
         //sex это пол
-        char sex;
+        char sex = 'F';
 
         //height это рост
         //по задаче измерение в метрах, поэтому float с возможностью десятичных дробей
-        float height;
+        float height = 170;
 
         //bricksAmount это количество кирпичей для постройки дома (небольшой одноэтажный дом площадью 100м2)
         short bricksAmount;
 
         //bactAmount это количество бактерий (измерять будем в КОЕ / г - такую величину можно встретить если почитать состав обычного йогурта,
         // часто там можно встретить число 1 * 107 КОЕ / г)
-        float bactAmount;
+        float bactAmount = 379645;
 
         //spaceDistance это расстояние до космических объектов (измерять будем в км)
-        double spaceDistance;
+        double spaceDistance = 10e32;
 
         //workLicense это разрешение на работу (рассматриваем всего 2 значения - или есть разрешение или его нет)
-        boolean workLicense;
+        //boolean workLicense = false;
 
         //fileSize это размер файла (измерять будем в байтах)
-        double fileSize;
+        double fileSize = 36589;
     }
 }
